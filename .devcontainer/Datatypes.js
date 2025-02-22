6beb342ecdfdc58cbafa6d7d956b36460a392af6
Saikat Mishra <saikatmishra1692005@gmail.com>
2025-02-22 08:47:27 +0000

    Datatypes

---
+++ .devcontainer/Datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version.
+//alert(3+3) // we are using node.js, not browser
+
+console.log(3+3);
+
+console.log("Saikat")
+//prettier -code formatter , code readability should be high 
+// documentation- alert MDN , ** tc39.es(global objects,.., number and detes.., how to write ...)
+
+
+let name="Abhi"
+let age= 18
+let isloogein= false
+let state =null
+
+//number-> 2 to the power 53 ,
+// bigint,
+// string->"", 
+// bollean-> true/ false
+//null-> standlone value
+// undefind-> 
+//symbol -> uniquess
+//* object
+
+console.log(typeof " saikat");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);//undefind
+
+// study documentation
+
+//Terminal open -> ctrl + shift + c
+//Run -> enter
